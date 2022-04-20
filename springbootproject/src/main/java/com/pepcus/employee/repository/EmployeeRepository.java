7158c71c5e92d37491514e9fcab09a3239a58035
gajendrathakue-pepcus <102954349+gajendrathakue-pepcus@users.noreply.github.com>
2022-04-20 14:59:53 +0530

    Add files via upload

---
+++ springbootproject/src/main/java/com/pepcus/employee/repository/EmployeeRepository.java
@@ -0,0 +1,20 @@
+package com.pepcus.employee.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import com.pepcus.employee.modal.Employee;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
+
+  @Query("from Employee ORDER BY name ASC")
+  List<Employee> findAllOrderByNameAsc();
+
+  @Query("SELECT e FROM Employee e WHERE e.name LIKE %:name%")
+  List<Employee> filterByName(@Param("name") String name);
+
+}
